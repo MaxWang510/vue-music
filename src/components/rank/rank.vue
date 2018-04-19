fc45b9a941857d14ddd67958552e90bcea9a173e
MaxWang510 <maxwang510@163.com>
2018-04-19 15:26:08 +0800

    头部和tab页面完成

---
+++ src/components/rank/rank.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+排行页面
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  data () {
+    return {
+
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
---
+++ src/components/singer/singer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    歌手页面
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  data () {
+    return {
+
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
